aa803ffdd3cd4b209092cb624066ff69551fde7c
Domagoj Rukavina <domagoj.rukavina@shoutem.com>
2017-06-05 16:51:34 +0200

    Implemented chokidar for file watcher

--- scripts/watch-local-extensions.js
+++ scripts/watch-local-extensions.js
@@ -2,65 +2,91 @@
 
 const path = require('path');
 const fs = require('fs-extra');
-const watch = require('node-watch');
-const globToRegExp = require('glob-to-regexp');
 const parseGitignore = require('parse-gitignore');
 const _ = require('lodash');
+const readline = require('readline');
+const chokidar = require('chokidar');
 
 const getLocalExtensions = require('./helpers/get-local-extensions.js');
 const configJsonPath = path.resolve('config.json');
 
-const COPY_DELAY_MS = 200;
+const NODE_MODULES = 'node_modules';
 
-function getIgnoreListForPath(folder) {
-  const gitignorePatterns = parseGitignore(path.join(folder, '.gitignore'));
-  const npmignorePatterns = parseGitignore(path.join(folder, '.npmignore'));
+function getIgnoreListForExtension(extensionPath) {
+  const gitignorePatterns = parseGitignore(path.join(extensionPath, '.gitignore'));
+  const npmignorePatterns = parseGitignore(path.join(extensionPath, '.npmignore'));
+
   return _.union(gitignorePatterns, npmignorePatterns, '.git');
-}
-
-function copyFile(src, dest) {
-  console.log(`Copying \nsrc: ${src} \ndst: ${dest}`);
-  fs.copy(src, dest, (error) => {
-    if (error) {
-      return console.error(error);
-    }
-  });
-}
-
-function copyFileDelayed(src, dest) {
-  _.debounce(() => copyFile(src, dest), COPY_DELAY_MS)();
 }
 
 function watchWorkingDirectories() {
   const config = fs.readJsonSync(configJsonPath);
   const localExtensions = getLocalExtensions(config.workingDirectories);
-  localExtensions.forEach((extension) => {
-    const packageName = extension.id;
-    const packagePath = extension.path;
-    const nodeModules = 'node_modules';
-    const ignoreList = getIgnoreListForPath(packagePath);
-    const installedExtensionPath = path.join(nodeModules, packageName);
-    const shouldCopyFile = (filePath) =>
-      !_.some(ignoreList, (ignorePath) =>
-         globToRegExp(path.join(packagePath, ignorePath)).test(filePath)
-      );
 
-    console.log(`Watching: ${packageName}`);
-    watch(packagePath, (filename) => {
-      const localPath = path.relative(packagePath, filename);
-      const destination = path.join(installedExtensionPath, localPath);
+  localExtensions.forEach(extension => {
+    const extensionName = extension.id;
+    const extensionPath = extension.path;
 
-      if (shouldCopyFile(filename)) {
-        copyFileDelayed(filename, destination);
-      }
+    const watcher = chokidar.watch(extensionPath, {
+      ignored: getIgnoreListForExtension(extensionPath),
+      persistent: true,
     });
+
+    watcher
+      .on('ready', () => {
+        console.log(`Watching: ${extensionName}`);
+      })
+      .on('add', addPath => {
+        const srcRelativePath = path.relative(extensionPath, addPath);
+        const dstPath = path.join(NODE_MODULES, extensionName, srcRelativePath);
+
+        fs.copy(addPath, dstPath)
+          .then(() => console.log(`ADD:${dstPath}`))
+          .catch(err => console.error(err))
+      })
+      .on('unlink', removePath => {
+        const srcRelativePath = path.relative(extensionPath, removePath);
+        const dstPath = path.join(NODE_MODULES, extensionName, srcRelativePath);
+
+        fs.remove(dstPath)
+          .then(() => console.log(`DEL:${dstPath}`))
+          .catch(err => console.error(err))
+      })
+      .on('change', changedPath => {
+        const srcRelativePath = path.relative(extensionPath, changedPath);
+        const dstPath = path.join(NODE_MODULES, extensionName, srcRelativePath);
+
+        fs.copy(changedPath, dstPath)
+          .then(() => console.log(`CP: \n${changedPath} to \n${dstPath}`))
+          .catch(err => console.error(err))
+      })
+      .on('error', error => log(`Watcher error: ${error}`));
+  });
+}
+
+function supportGracefulShutdownOnWindows() {
+  if (/^win/.test(process.platform)) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.on('SIGINT', function () {
+      process.emit('SIGINT');
+    });
+  }
+
+  process.on('SIGINT', function () {
+    process.exit();
   });
 }
 
 // Initial watch for working directories
 watchWorkingDirectories();
+// adds SIGINT (CTRL+C) fix on windows
+supportGracefulShutdownOnWindows();
 
 // Watch changes on config.json and trigger build and re-watch for working directories
-watch(configJsonPath, () => {
+chokidar.watch(configJsonPath, () => {
   console.error('config.json changed. You should rebuild the app!');
 });
