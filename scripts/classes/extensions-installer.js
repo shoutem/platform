16db2485e44c5c953aef6d5d617517f56415f934
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2020-07-13 17:56:42 +0200

    Bump platform version and add npm fallback to yarn install

--- scripts/classes/extensions-installer.js
+++ scripts/classes/extensions-installer.js
@@ -19,10 +19,14 @@ function addDependencyToPackageJson(packageJson, name, version) {
 function installJsDependencies() {
   console.log('Installing dependencies:'.bold);
 
-  return spawn('yarn', ['install'], {
-    stderr: 'inherit',
-    stdio: 'inherit',
-  });
+  const yarnCheckCommand = 'yarn -v';
+  const yarnExists = shell.exec(yarnCheckCommand).code === 0;
+  const stdArgs = { stderr: 'inherit', stdio: 'inherit' };
+
+  // use yarn if it exists, otherwise use npm (this is so 3rd party devs aren't
+  // forced to install yarn just for this one step in our configuration script)
+  return yarnExists ? spawn('yarn', ['install'], stdArgs) :
+    spawn('npm', ['install'], stdArgs);
 }
 
 function installNpmExtension(extension) {
