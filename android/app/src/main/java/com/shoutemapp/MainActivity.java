474c08af0bc91a1ac49ba4bf7846290aa2f3dd4b
Slavko <slavko.stimac@fiveminutes.eu>
2021-11-24 09:50:00 +0100

    android back handling and auth version bump

--- android/app/src/main/java/com/shoutemapp/MainActivity.java
+++ android/app/src/main/java/com/shoutemapp/MainActivity.java
@@ -13,6 +13,12 @@ public class MainActivity extends ReactActivity {
         super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        // do not call super. invokeDefaultOnBackPressed() as it will close the app.  Instead lets just put it in the background.
+        moveTaskToBack(true);
+    }
+
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
