47e09b3c63128096582e8df1f012639a6622838c
Definitely-Not-Vlad <vlad@definitely-not-vlad.com>
2022-03-09 09:47:18 +0100

    Remove vector-icons, update scripts, update platform.json deps

--- scripts/generate-translation-file.js
+++ scripts/generate-translation-file.js
@@ -1,20 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
+const path = require('path');
 
-const EXT_PATH_ROOT = '../extensions/';
-const PLATFORM_PATH = '../platform/platform.json';
-const TRANSLATION_PATH = '/app/translations/en.json';
+const EXT_PATH_ROOT = path.join('..', 'extensions');
 
-// returns array of extensions from platform.json dependencies
-function fetchPlatformExtensions() {
-  const platformJson = fs.readJsonSync(PLATFORM_PATH);
+const IGNORED_EXTENSIONS = ['i18n'];
 
-  return Object.keys(platformJson.dependencies);
+function fetchAllExtensions() {
+  const listOfExtensions = fs
+    .readdirSync(EXT_PATH_ROOT)
+    .filter(file => fs.lstatSync(path.join(EXT_PATH_ROOT, file)).isDirectory());
+
+  return listOfExtensions;
 }
 
 function getExtensionTranslations(extTitle) {
-  const translationPath = `${EXT_PATH_ROOT}/shoutem.${extTitle}${TRANSLATION_PATH}`;
+  if (IGNORED_EXTENSIONS.includes(extTitle)) {
+    return null;
+  }
+
+  const translationPath = path.join(
+    EXT_PATH_ROOT,
+    `shoutem.${extTitle}`,
+    'app',
+    'translations',
+    'en.json',
+  );
 
   if (fs.existsSync(translationPath)) {
     return fs.readJsonSync(translationPath);
@@ -25,7 +37,7 @@ function getExtensionTranslations(extTitle) {
 
 function generateTranslationFile() {
   let allTranslations = {};
-  const extensions = fetchPlatformExtensions();
+  const extensions = fetchAllExtensions();
 
   extensions.forEach(extension => {
     const extTitle = extension.replace('shoutem.', '');
