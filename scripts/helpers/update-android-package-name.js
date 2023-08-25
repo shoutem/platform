f370994fdba24d9250daa9d4b2e16a813358f5cf
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2023-08-25 12:46:48 +0200

    Remove obsolete newarchitecture dir and scripts for editing it

--- scripts/helpers/update-android-package-name.js
+++ scripts/helpers/update-android-package-name.js
@@ -3,9 +3,6 @@ const fs = require('fs-extra');
 const getAppGradlePath = require('./get-app-gradle-path');
 const getMainActivityPath = require('./get-main-activity-path');
 const getMainApplicationPath = require('./get-main-application-path');
-const getMainApplicationReactNativeHostPath = require('./get-main-application-react-native-host-path');
-const getMainComponentsRegistryPath = require('./get-main-components-registry-path');
-const getMainApplicationTurboModuleManagerDelegatePath = require('./get-main-application-turbo-module-manager-delegate-path');
 
 function getSearchReplaceFiles(oldPackageName, newPackageName) {
   return [
@@ -26,30 +23,6 @@ function getSearchReplaceFiles(oldPackageName, newPackageName) {
       search: `package ${oldPackageName};`,
       replace: `package ${newPackageName};`,
       fileName: 'MainApplication.java',
-    },
-    {
-      path: getMainApplicationReactNativeHostPath(),
-      search: `package ${oldPackageName};`,
-      replace: `package ${newPackageName};`,
-      fileName: 'MainApplicationReactNativeHost.java',
-    },
-    {
-      path: getMainApplicationReactNativeHostPath(),
-      search: `import ${oldPackageName}`,
-      replace: `import ${newPackageName}`,
-      fileName: 'MainApplicationReactNativeHost.java',
-    },
-    {
-      path: getMainComponentsRegistryPath(),
-      search: `package ${oldPackageName};`,
-      replace: `package ${newPackageName};`,
-      fileName: 'MainComponentsRegistry.java',
-    },
-    {
-      path: getMainApplicationTurboModuleManagerDelegatePath(),
-      search: `package ${oldPackageName};`,
-      replace: `package ${newPackageName};`,
-      fileName: 'MainApplicationTurboModuleManagerDelegate.java',
     },
   ];
 }
