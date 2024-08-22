850b97a8afd8ea213040547d1774f7e780919029
sstimac <sstimac.contact@gmail.com>
2024-08-22 14:36:41 +0200

    webpack changes and version bump

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,14 +2,9 @@ const path = require('path');
 const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 
 const appDirectory = path.resolve(__dirname, '../');
-
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProduction = nodeEnv === 'production';
-const isDev = nodeEnv === 'development';
 
 // This is needed for webpack to compile JavaScript.
 // Many OSS React Native packages are not compiled to ES5 before being
@@ -68,42 +63,44 @@ const svgImportLoaderConfiguration = {
   use: [{ loader: '@svgr/webpack', options: { dimensions: false } }],
 }
 
-const styleRules = [
-  {
-    test: /\.css$/,
-    use: [
-      !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
-      { loader: 'css-loader' },
-    ],
-  },
-  {
-    test: /\.scss$/,
-    use: [
-      !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
-        options: {
-          sourceMap: !isProduction,
-        },
-      },
-      {
-        loader: 'postcss-loader',
-        options: {
-          postcssOptions:{
-            plugins: [require('cssnano')({ preset: 'default' })], 
+function styleRules(isProduction) {
+  return [
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        { loader: 'css-loader' },
+      ],
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: !isProduction,
           },
-          sourceMap: !isProduction,
         },
-      },
-      {
-        loader: 'sass-loader',
-        options: {
-          sourceMap: !isProduction,
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions:{
+              plugins: [require('cssnano')({ preset: 'default' })], 
+            },
+            sourceMap: !isProduction,
+          },
         },
-      },
-    ],
-  },
-];
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: !isProduction,
+          },
+        },
+      ],
+    },
+  ];
+}
 
 const tsLoaderConfiguration = {
   test: /\.(ts)x?$/,
@@ -132,78 +129,83 @@ const fontLoaderConfiguration = {
   ],
 };
 
-module.exports = {
-  entry: [
-    // load any web API polyfills
-    // path.resolve(appDirectory, 'polyfills-web.js'),
-    // your web-specific entry file
-    path.resolve(appDirectory, 'index.web.js')
-  ],
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const isDev = argv.mode === 'development';
 
-  // configures where the build ends up
-  output: {
-    filename: 'bundle.web.js',
-    path: path.resolve(appDirectory, 'dist'),
-    publicPath: '',
-  },
+  return {
+    entry: [
+      // load any web API polyfills
+      // path.resolve(appDirectory, 'polyfills-web.js'),
+      // your web-specific entry file
+      path.resolve(appDirectory, 'index.web.js')
+    ],
 
-  // DEV Only
-  ...(isDev ? {
-    devtool: 'source-map'
-  } : {}),
-
-  module: {
-    rules: [
-      babelLoaderConfiguration,
-      imageLoaderConfiguration,
-      tsLoaderConfiguration,
-      htmlLoaderConfiguration,
-      svgImportLoaderConfiguration,
-      fontLoaderConfiguration,
-      ...styleRules
-    ]
-  },
-
-  resolve: {
-    // This will only alias the exact import "react-native"
-    alias: {
-      //NativeModuleInjectionMark-webpack-config-resolve-alias
-
-      'react-native$': 'react-native-web',
-      'shoutem-core$': path.resolve(appDirectory, 'core'),
-
-      // Solving the problem in general ( no extension ownership )
-      'react-native-fast-image': 'react-native-web/dist/exports/Image',
-      'react-native/Libraries/vendor/emitter/EventEmitter': 'react-native-web/dist/vendor/react-native/EventEmitter/RCTDeviceEventEmitter.js',
+    // configures where the build ends up
+    output: {
+      filename: 'bundle.web.js',
+      path: path.resolve(appDirectory, 'dist'),
+      publicPath: '',
     },
-    // If you're working on a multi-platform React Native app, web-specific
-    // module implementations should be written in files using the extension
-    // `.web.js`.
-    extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", '.web.js', '.js']
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "index.html"),
-      inject: "body"
-    }),
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(isDev),
-    }),
-
-    // Reanimated
-    new webpack.EnvironmentPlugin({ JEST_WORKER_ID: null }),
-    new webpack.DefinePlugin({ process: { env: {} } }),
 
     // DEV Only
-    ...(isDev ? [new webpack.HotModuleReplacementPlugin()] : []),
+    ...(isDev ? {
+      devtool: 'source-map'
+    } : {}),
 
-    // PROD Only
-    ...(isProduction ? [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1,
+    module: {
+      rules: [
+        babelLoaderConfiguration,
+        imageLoaderConfiguration,
+        tsLoaderConfiguration,
+        htmlLoaderConfiguration,
+        svgImportLoaderConfiguration,
+        fontLoaderConfiguration,
+        ...styleRules(isProduction),
+      ]
+    },
+
+    resolve: {
+      // This will only alias the exact import "react-native"
+      alias: {
+        //NativeModuleInjectionMark-webpack-config-resolve-alias
+
+        'react-native$': 'react-native-web',
+        'shoutem-core$': path.resolve(appDirectory, 'core'),
+
+        // Solving the problem in general ( no extension ownership )
+        'react-native-fast-image': 'react-native-web/dist/exports/Image',
+        'react-native/Libraries/vendor/emitter/EventEmitter': 'react-native-web/dist/vendor/react-native/EventEmitter/RCTDeviceEventEmitter.js',
+      },
+      // If you're working on a multi-platform React Native app, web-specific
+      // module implementations should be written in files using the extension
+      // `.web.js`.
+      extensions: [".web.tsx", ".web.ts", ".tsx", ".ts", '.web.js', '.js']
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "index.html"),
+        inject: "body"
       }),
-      new HtmlInlineScriptPlugin()
-    ] : []),
-  ]
+      new webpack.DefinePlugin({
+        __DEV__: JSON.stringify(isDev),
+      }),
+
+      // Reanimated
+      new webpack.EnvironmentPlugin({ JEST_WORKER_ID: null }),
+      new webpack.DefinePlugin({ process: { env: {} } }),
+
+      // DEV Only
+      ...(isDev ? [new webpack.HotModuleReplacementPlugin()] : []),
+
+      // PROD Only
+      ...(isProduction ? [
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1,
+        }),
+        new HtmlInlineScriptPlugin()
+      ] : []),
+    ]
+  }
 }
