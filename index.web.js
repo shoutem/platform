baa695e5b40224df3580828b9a58b8a1e353ef23
sstimac <sstimac.contact@gmail.com>
2024-09-13 14:22:25 +0200

    latest changes

--- index.web.js
+++ index.web.js
@@ -10,6 +10,20 @@ import extensions from './extensions.js';
 // Web styling
 import '@shoutem/ui/web/style/Alert.scss';
 
+// Supress current legacy errors
+const consoleError = console.error;
+const SUPPRESSED_WARNINGS = [
+  'childContextTypes API which is no longer supported',
+  'Support for defaultProps will be removed',
+  'legacy contextTypes API which is no longer supported',
+];
+
+console.error = function filterWarnings(msg, ...args) {
+  if (!SUPPRESSED_WARNINGS.some(entry => msg.includes(entry))) {
+    consoleError(msg, ...args);
+  }
+};
+
 // WebInjectionMark-font-styles
 
 // WebInjectionMark-create-stylesheet
