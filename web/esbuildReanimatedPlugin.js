c6ef432c79332578f6fea0f9ad281123314bc706
sstimac <sstimac.contact@gmail.com>
2025-04-02 16:03:10 +0200

    changes & versions

--- web/esbuildReanimatedPlugin.js
+++ web/esbuildReanimatedPlugin.js
@@ -1,6 +1,7 @@
 const generate = require('@babel/generator').default;
 const babelParser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
+const t = require('@babel/types');
 const fs = require('fs').promises;
 
 module.exports = function reanimatedPlugin() {
@@ -10,11 +11,7 @@ module.exports = function reanimatedPlugin() {
       build.onLoad({ filter: /\.(js|jsx|ts|tsx)$/ }, async args => {
         const code = await fs.readFile(args.path, 'utf8');
 
-        // Skip files that don't have 'react-native-reanimated' in path or in code
-        if (
-          !args.path.includes('react-native-reanimated') &&
-          !/import\s.*react-native-reanimated/.test(code)
-        ) {
+        if (!/['"]react-native-reanimated['"]/.test(code)) {
           return;
         }
 
@@ -23,62 +20,71 @@ module.exports = function reanimatedPlugin() {
           plugins: ['jsx', 'typescript'],
         });
 
+        const reanimatedImports = new Set();
+
         traverse(ast, {
-          CallExpression(path) {
-            const { node } = path;
-            const { callee } = node;
-
-            if (
-              callee.type === 'Identifier' &&
-              [
-                'useDerivedValue',
-                'useAnimatedStyle',
-                'useAnimatedProps',
-                'useAnimatedReaction',
-              ].includes(callee.name)
-            ) {
-              const callback = node.arguments[0];
-
-              if (callback && callback.type === 'ArrowFunctionExpression') {
-                const dependencies = new Set();
-
-                traverse(
-                  callback,
-                  {
-                    MemberExpression(innerPath) {
-                      const { node: innerNode } = innerPath;
-                      if (
-                        innerNode.object &&
-                        innerNode.object.type === 'Identifier' &&
-                        innerNode.property &&
-                        innerNode.property.name === 'value'
-                      ) {
-                        dependencies.add(innerNode.object.name);
-                      }
-                    },
-                  },
-                  path.scope,
-                  path.parentPath,
-                );
-
-                if (
-                  node.arguments.length < 2 ||
-                  node.arguments[1].type !== 'ArrayExpression'
-                ) {
-                  node.arguments.push({
-                    type: 'ArrayExpression',
-                    elements: Array.from(dependencies).map(dep => ({
-                      type: 'Identifier',
-                      name: dep,
-                    })),
-                  });
+          ImportDeclaration(path) {
+            if (path.node.source.value === 'react-native-reanimated') {
+              for (const spec of path.node.specifiers) {
+                if (t.isImportSpecifier(spec)) {
+                  reanimatedImports.add(spec.local.name);
                 }
               }
             }
           },
         });
 
-        // Generate the modified code
+        if (reanimatedImports.size === 0) {
+          return;
+        }
+
+        traverse(ast, {
+          CallExpression(path) {
+            const { node } = path;
+            const callee = node.callee;
+
+            if (
+              t.isIdentifier(callee) &&
+              reanimatedImports.has(callee.name)
+            ) {
+              const callback = node.arguments[0];
+
+              if (t.isArrowFunctionExpression(callback) || t.isFunctionExpression(callback)) {
+                const dependencies = new Set();
+
+                // Only collect the object identifiers used like foo.value
+                traverse(callback.body, {
+                  MemberExpression(innerPath) {
+                    const innerNode = innerPath.node;
+                    if (
+                      t.isIdentifier(innerNode.object) &&
+                      t.isIdentifier(innerNode.property) &&
+                      innerNode.property.name === 'value'
+                    ) {
+                      dependencies.add(innerNode.object.name);
+                    }
+                  },
+                }, path.scope);
+
+                if (
+                  node.arguments.length < 2 ||
+                  !t.isArrayExpression(node.arguments[1])
+                ) {
+                  const depArray = t.arrayExpression(
+                    Array.from(dependencies).map(name => t.identifier(name))
+                  );
+
+                  if (node.arguments.length === 1) {
+                    node.arguments.push(depArray);
+                  } else {
+                    node.arguments[1] = depArray;
+                  }
+                }
+              }
+            }
+          },
+        });
+
         const output = generate(ast, {}, code);
 
         return {
