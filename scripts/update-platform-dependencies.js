df972f874b5e5428975b1556ae442756b11b6a99
tomislav-arambasic <tomislav.arambasic@gmail.com>
2025-04-25 09:37:48 +0200

    update script

--- scripts/update-platform-dependencies.js
+++ scripts/update-platform-dependencies.js
@@ -8,4 +8,15 @@ const platformPath = path.resolve(path.join('../platform', 'platform.json'));
 const platformJson = fs.readJsonSync(platformPath);
 
 platformJson.dependencies = getPlatformDependencies(process.argv[2]);
+
+// Sync versions from dependencies
+// eslint-disable-next-line arrow-parens
+Object.keys(platformJson.requiredDependencies).forEach(extensionName => {
+  if (platformJson.dependencies[extensionName]) {
+    // eslint-disable-next-line operator-linebreak
+    platformJson.requiredDependencies[extensionName] =
+      platformJson.dependencies[extensionName];
+  }
+});
+
 fs.writeJsonSync(platformPath, platformJson, { spaces: 2 });
